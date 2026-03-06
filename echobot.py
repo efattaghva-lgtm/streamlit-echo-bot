0c5fd9f672f0f81c30212753dcd9a523fa857cd9
efattaghva-lgtm <efat.taghva@gmail.com>
2026-03-06 11:05:53 -0800

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,3 +1,7 @@
-import streamlit as st 
-with st.chat_message("assistant"): 
-    st.write("hello, how's it going 👋") 
+import streamlit as st
+import numpy as np
+
+with st.chat_message("assistant"):
+    st.write("Hello human")
+    st.bar_chart(np.random.randn(30, 3))
+
